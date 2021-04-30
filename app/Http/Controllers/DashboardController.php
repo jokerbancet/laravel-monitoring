01855aa89cd4e2a024db840de2b2941166d174eb
jokerbancet <jokerbancet@users.noreply.github.com>
2021-04-30 11:34:42 +0700

    Update counter dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        $data = DB::table('mahasiswa')->get();
+        $data2 = DB::table('dosenpembimbing')->get();
+        $data3 = DB::table('pembimbingindustri')->get();
+        $data4 = DB::table('industri')->get();
+        // dd($data);
+        return view('dashboard.index', [
+            'data' => $data,
+            'data2' => $data2,
+            'data3' => $data3,
+            'data4' => $data4
+            ]);
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -16,7 +16,7 @@
                             <div class="metric">
                                 <span class="icon"><i class="fa fa-user"></i></span>
                                 <p>
-                                    <span class="number">1</span>
+                                    <span class="number">{{ $data->count()}}</span>
                                     <span class="title">Total Mahasiswa</span>
                                 </p>
                             </div>
@@ -25,7 +25,7 @@
                             <div class="metric">
                                 <span class="icon"><i class="fa fa-user"></i></span>
                                 <p>
-                                    <span class="number">1</span>
+                                    <span class="number">{{$data2->count()}}</span>
                                     <span class="title">Total Dosen Pembimbing</span>
                                 </p>
                             </div>
@@ -34,7 +34,7 @@
                             <div class="metric">
                                 <span class="icon"><i class="fa fa-user"></i></span>
                                 <p>
-                                    <span class="number">1</span>
+                                    <span class="number">{{$data3->count()}}</span>
                                     <span class="title">Total Pembimbing Industri</span>
                                 </p>
                             </div>
@@ -43,7 +43,7 @@
                             <div class="metric">
                                 <span class="icon"><i class="fa fa-industry"></i></span>
                                 <p>
-                                    <span class="number">1</span>
+                                    <span class="number">{{$data4->count()}}</span>
                                     <span class="title">Total Industri</span>
                                 </p>
                             </div>
@@ -51,75 +51,6 @@
                     </div>
                 </div>
             </div>
-            {{-- <div class="row">
-                <div class="col-md-6">
-                    <div class="panel">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Recent Purchases</h3>
-                            <div class="right">
-                                <button type="button" class="btn-toggle-collapse"><i class="lnr lnr-chevron-up"></i></button>
-                                <button type="button" class="btn-remove"><i class="lnr lnr-cross"></i></button>
-                            </div>
-                        </div>
-                        <div class="panel-body no-padding">
-                            <table class="table table-striped">
-                                <thead>
-                                    <tr>
-                                        <th>Order No.</th>
-                                        <th>Name</th>
-                                        <th>Amount</th>
-                                        <th>Date &amp; Time</th>
-                                        <th>Status</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td><a href="#">763648</a></td>
-                                        <td>Steve</td>
-                                        <td>$122</td>
-                                        <td>Oct 21, 2016</td>
-                                        <td><span class="label label-success">COMPLETED</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="#">763649</a></td>
-                                        <td>Amber</td>
-                                        <td>$62</td>
-                                        <td>Oct 21, 2016</td>
-                                        <td><span class="label label-warning">PENDING</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="#">763650</a></td>
-                                        <td>Michael</td>
-                                        <td>$34</td>
-                                        <td>Oct 18, 2016</td>
-                                        <td><span class="label label-danger">FAILED</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="#">763651</a></td>
-                                        <td>Roger</td>
-                                        <td>$186</td>
-                                        <td>Oct 17, 2016</td>
-                                        <td><span class="label label-success">SUCCESS</span></td>
-                                    </tr>
-                                    <tr>
-                                        <td><a href="#">763652</a></td>
-                                        <td>Smith</td>
-                                        <td>$362</td>
-                                        <td>Oct 16, 2016</td>
-                                        <td><span class="label label-success">SUCCESS</span></td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div class="panel-footer">
-                            <div class="row">
-                                <div class="col-md-6"><span class="panel-note"><i class="fa fa-clock-o"></i> Last 24 hours</span></div>
-                                <div class="col-md-6 text-right"><a href="#" class="btn btn-primary">View All Purchases</a></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div> --}}
         </div>
     </div>
 </div>
