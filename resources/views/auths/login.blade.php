426a97af93f3281087bb971b19b62b4bb9d22a78
Dony <72926343+jokerbancet@users.noreply.github.com>
2022-01-04 10:25:22 +0700

    branding2

--- resources/views/auths/login.blade.php
+++ resources/views/auths/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" class="fullscreen-bg">
 
 <head>
-	<title>Aplikasi Monitoring Magang Mahasiswa | PEP Bandung</title>
+	<title>Aplikasi MERDEKA (Media Rekam Data Elektronik Praktik Kerja) | PEP Bandung</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
